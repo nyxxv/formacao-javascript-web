c74d86b416e3b7e2e3c89c6c12d7337227d99da6
Ligya Augusto <ligya.and@gmail.com>
2024-03-01 17:46:35 -0300

    melhorias de acessibilidade

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-function tocarSom (idAudioTecla) { 
-   document.querySelector(idAudioTecla).play();
+function tocarSom (seletorAudio) { 
+   const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === "audio") {
+        elemento.play();
+   } else {
+        console.log("Elemento não encontrado ou seletor inválido");
+   }
 }
 
 // Essa é a lista de todas as teclas que estão no HTML 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-while(contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    
     const idAudio = `#som_${instrumento}`;
+    
     tecla.onclick = function () {
-        tocarSom(idAudio)
+        tocarSom(idAudio);
     } ;
-    contador++; 
+
+    tecla.onkeydown = function (evento) {
+
+       if(evento.code === "Space" || evento.code === "Enter") {
+        tecla.classList.add("ativa");
+       } 
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove("ativa"); 
+    }
+
 }
